7ff6abd16c802a00063902141e75ef76255b69c9
Benjamin Cook <bengcook55@gmail.com>
2022-11-11 21:45:01 -0500

    commit tree 03540fa07a83ba3bef50ebabdaf8f00d0d67290d

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: 'scott',
+    streetAddress: 'prior 310',
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newObject = {...obj};
+    newObject[key] = value;
+    return newObject;
+}
+
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObject = {...employee};
+    delete newObject[key];
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
